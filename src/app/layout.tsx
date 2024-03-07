ac016106fe6b17ed82df135f6e017b7061ae984a
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-03-07 18:32:16 +0100

    tailwind merge installation and setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/twMerge";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={ inter.className }>
+        <html lang="es">
+            <body className={ cn(inter.className, 'min-h-screen min-w-screen bg-slate-950 text-white') }>
                 { children }
             </body>
         </html>
