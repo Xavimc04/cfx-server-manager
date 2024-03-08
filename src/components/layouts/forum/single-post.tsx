0fff4980afc4e9d83758fc8e1b5c234511f44096
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-03-08 19:03:26 +0100

    Link to single post

--- src/components/layouts/forum/single-post.tsx
+++ src/components/layouts/forum/single-post.tsx
@@ -1,29 +1,34 @@
 import { Post } from "@/types/forum/_types";
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import Link from "next/link";
 
 export default function SinglePost({
     post
 } : {
     post: Post
 }) {
-    return <article className="border border-zinc-700 p-3 rounded flex items-center gap-4">
-        {/* @ Image */}
-        <img 
-            src={ post.author.image }
-            alt={ post.author.name }
-            className="w-10 h-10 rounded-md"
-        />
-        
-        {/* @ Information */}
-        <section className="flex-1">
-            <h2 className="poppins">{ post.title }</h2>
+    return <Link
+        href={ `/forum/post/${ post.id }` }
+    >
+        <article className="border border-zinc-700 p-3 rounded flex items-center gap-4">
+            {/* @ Image */}
+            <img 
+                src={ post.author.image }
+                alt={ post.author.name }
+                className="w-10 h-10 rounded-md"
+            />
+            
+            {/* @ Information */}
+            <section className="flex-1">
+                <h2 className="poppins">{ post.title }</h2>
 
-            <p className="text-sm text-gray-400">{ post.author.name }, { post.createdAt.toDateString() }</p>
-        </section>
+                <p className="text-sm text-gray-400">{ post.author.name }, { post.createdAt.toDateString() }</p>
+            </section>
 
-        {/* @ Stats */}
-        <section>
-            <StarBorderOutlinedIcon />
-        </section>
-    </article>
+            {/* @ Stats */}
+            <section>
+                <StarBorderOutlinedIcon />
+            </section>
+        </article>
+    </Link>
 }
