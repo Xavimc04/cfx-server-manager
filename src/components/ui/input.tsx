9910f03f948ca73115e2c6a55ceee2d89f994e7a
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-03-08 16:21:11 +0100

    Reusable input and label inputs

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { cn } from "@/lib/twMerge"
+import { InputProps } from "@/types/ui/input/_types"
+
+export default function Input(props: InputProps) {
+    return <input 
+        type={props.type || 'text'} 
+        placeholder={props.placeholder} 
+        className={cn(
+            "px-4 text-sm py-2.5 flex items-center gap-3 rounded-lg focus:outline-none focus:ring-0 bg-gray-50 dark:bg-black/30 border dark:border-gray-600 transition-all focus:border-indigo-500", 
+            props.className
+        )} 
+        onChange={props.onChange} 
+        value={props.value} 
+        required={props.required} 
+        disabled={props.disabled} 
+        autoComplete={props.autoComplete} 
+        autoFocus={props.autoFocus} 
+        id={props.id} 
+        name={props.name} 
+        onBlur={props.onBlur}
+        onFocus={props.onFocus}
+    />
+}
---
+++ src/types/ui/input/_types.ts
@@ -0,0 +1,16 @@
+export interface InputProps {
+    type?: string;
+    placeholder?: string;
+    className?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    value?: string;
+    initialValue?: string;
+    required?: boolean;
+    disabled?: boolean;
+    autoComplete?: string;
+    autoFocus?: boolean;
+    id?: string;
+    name?: string; 
+    onBlur?: () => void;
+    onFocus?: () => void;
+}
