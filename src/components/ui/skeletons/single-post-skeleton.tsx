5832041a32cf69d454a47620078865d0f7d72f36
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-03-12 18:03:09 +0100

    User skeleton switch to reusable ui component

--- src/components/ui/skeletons/single-post-skeleton.tsx
+++ src/components/ui/skeletons/single-post-skeleton.tsx
@@ -1,18 +1,8 @@
+import UserInformationSkeleton from "./user-information-skeleton";
+
 export default function SinglePostSkeleton() {
     return <section className="w-1/2 flex flex-col gap-4">
-        <div className="flex gap-4 items-center mb-5">
-            <div className="animate-pulse bg-zinc-800 w-16 h-16 rounded-md"></div>
-            
-            <div className="flex-1 flex flex-col gap-2 py-1">
-                <div className="animate-pulse bg-zinc-800 w-2/4 h-4 rounded-md"></div>
-                <div className="animate-pulse bg-zinc-800 w-1/4 h-3 rounded-md"></div>
-            </div>
-
-            <div className="flex items-center gap-4">
-                <div className="animate-pulse bg-zinc-800 w-10 h-10 rounded-md"></div>
-                <div className="animate-pulse bg-zinc-800 w-10 h-10 rounded-md"></div>
-            </div>
-        </div>
+        <UserInformationSkeleton />
         
         <div className="animate-pulse bg-zinc-800 w-1/2 h-16 rounded-md"></div>
 
