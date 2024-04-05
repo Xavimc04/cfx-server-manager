9923dcd283f75bf3ae703a3c22510bec84c9184d
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-04-05 16:50:28 +0200

    New adventure section for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
         </section>
 
         {/* @ Background */}
-        <section className="h-screen -mt-36 relative -z-20"> 
+        <section className="h-screen -mt-36 relative -z-20 flex items-center justify-center"> 
             <Image
                 className="grayscale w-full h-full object-cover"
                 style={{
@@ -34,6 +34,31 @@ export default function Page() {
 
             <div className="h-20 w-screen absolute top-0 bg-gradient-to-b from-zinc-900"></div>
             <div className="h-20 w-screen absolute bottom-0 bg-gradient-to-t from-zinc-900"></div>
+        
+            <section className="z-20 w-1/2 flex items-center justify-between">
+                <article className="w-1/2 flex items-start flex-col gap-7">
+                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+                        ¿Estás listo para tu próxima
+                        
+                        <p className="underline underline-offset-3 decoration-8 decoration-blue-400">
+                            aventura?
+                        </p>
+                    </h1>
+                    
+                    <p className="text-lg font-normal text-gray-500 lg:text-xl">
+                        { process.env.APP_NAME } es un servidor de Roleplay en español basado en la ciudad de Los Santos, California. 
+                        Con una economía realista, trabajos, empresas, drogas, mafias, policías, médicos, abogados, y mucho más. 
+                        ¡Comienza tu aventura en { process.env.APP_NAME }!
+                    </p>
+                </article>
+
+                <Image
+                    src="/images/neon_rich.png"
+                    alt="Franklin"
+                    width={300}
+                    height={400}
+                />
+            </section>
         </section>
 
         <Footer />
